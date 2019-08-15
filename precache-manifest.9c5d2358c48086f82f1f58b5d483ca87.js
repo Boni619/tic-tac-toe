8d072425757ce2fbbed8946f2d14c05c3f53f435
Boni <boni619@gmail.com>
2019-08-15 12:12:12 +0530

    Updates

---
+++ precache-manifest.9c5d2358c48086f82f1f58b5d483ca87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ae4f48fde4ddfb128ea8a856a513c5",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "8e3245e169d90b2b529b",
+    "url": "/tic-tac-toe/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "33a6694a725e1b9e6972",
+    "url": "/tic-tac-toe/static/css/main.81972b9d.chunk.css"
+  },
+  {
+    "revision": "8e3245e169d90b2b529b",
+    "url": "/tic-tac-toe/static/js/2.5fa2e46d.chunk.js"
+  },
+  {
+    "revision": "33a6694a725e1b9e6972",
+    "url": "/tic-tac-toe/static/js/main.1e0bb2ca.chunk.js"
+  },
+  {
+    "revision": "8dcbae9973e4db2b37aa",
+    "url": "/tic-tac-toe/static/js/runtime~main.2c879db0.js"
+  }
+]);
